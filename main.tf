2b455ed344a736f952d83daf4ffacde14c469ebc
Chiedozie Chukwurah <dchukwurah@live.com>
2023-10-20 16:35:15 +0100

    amended main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_route_table" "public_rt" {
  	vpc_id = aws_vpc.vpc.id
 
  	route {
- 		cidr_block = var.cidr-public-rt
+ 		cidr_block = "0.0.0.0/0"
  		gateway_id = aws_internet_gateway.ig.id
  	}
  	tags = {
